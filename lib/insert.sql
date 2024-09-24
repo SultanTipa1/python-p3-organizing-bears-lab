ef301efe0a1cfd82551cb44a173a05fb6ad95338
SultanTipa1 <josephngumbaugush@gmail.com>
2024-09-24 07:57:13 +0300

    Adds all files

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 20, 'M', 'brown', 'friendly', 1),
+('Rowdy', 3, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'golden', 'curious', 1),
+('Sergeant Brown', 7, 'M', 'brown', 'calm', 1),
+('Melissa', 13, 'F', 'black', 'playful', 1),
+('Grinch', 2, 'M', 'green', 'grumpy', 1),
+('Wendy', 1, 'F', 'white', 'shy', 1),
+(NULL, 3, 'M', 'gray', 'mischievous', 1);  -- Use NULL here for unnamed
+
+
+
